1fd0bee4431aae616437ac0b7398b1b71e98e4af
Guillermo Sanchez Palacios <nothicc04@gmail.com>
2024-01-20 19:44:27 -0500

    advance learning

---
+++ java_note/src/poo/Workers/Docentes.java
@@ -0,0 +1,26 @@
+package poo.Workers;
+
+public abstract class Docentes extends Trabajador{
+  
+    private String Nomb_Dprt;
+  
+    public Docentes(String Nomb_Emp, int DNI, String Nomb_Dprt){
+      super (Nomb_Emp, DNI);
+      this.Nomb_Dprt = Nomb_Dprt;
+    }
+    
+    public String getNomb_Dprt(){
+      return Nomb_Dprt;
+    }
+  
+    public void setNomb_Dprt(String Nomb_Dprt){
+      this.Nomb_Dprt = Nomb_Dprt;
+    }
+
+    public abstract double calcularSalario();
+
+    public String toString() {
+      return "\n Nombre: " + super.getNomb_Emp() + "\n DNI: " + super.getDNI() + "\n Nombre del departamento: "+ Nomb_Dprt;
+    }
+}
+  
