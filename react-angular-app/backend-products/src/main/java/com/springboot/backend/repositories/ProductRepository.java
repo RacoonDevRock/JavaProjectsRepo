0cb703e9a28c8a49b32ebeb7c1f346d4c0aab2d0
Guillermo Sanchez Palacios <nothicc04@gmail.com>
2024-02-02 15:00:38 -0500

    backend-product-app

---
+++ react-angular-app/backend-products/src/main/java/com/springboot/backend/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.backend.repositories;
+
+import com.springboot.backend.entities.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "products")  // http://localhost:8080/products - GET
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
